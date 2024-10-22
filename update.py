8fea2cd783285f082a9b8f34907417c2312538e2
Syuhadak27 <117827583+Syuhadak27@users.noreply.github.com>
2024-10-22 21:25:36 +0700

    Update update.py

--- update.py
+++ update.py
@@ -86,7 +86,7 @@ if DATABASE_URL is not None:
 
 UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "")
 if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = None
+    UPSTREAM_REPO = "https://github.com/Syuhadak27/mltb-hk"
 
 UPSTREAM_BRANCH = environ.get("UPSTREAM_BRANCH", "")
 if len(UPSTREAM_BRANCH) == 0:
